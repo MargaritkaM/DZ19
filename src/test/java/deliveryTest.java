590cebeb0ae56105138fc74a3e90a2b90c19aac8
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-09-30 10:08:34 +0300

    First commit

---
+++ src/test/java/deliveryTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class deliveryTest {
+
+
+    @Test
+    void test (){
+    open ("http://localhost:9999/");
+
+
+    }
+
+}
