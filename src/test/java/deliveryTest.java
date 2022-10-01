40b09b18e03453e1e5cd8158a8d847936d4c3039
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-10-01 14:33:57 +0300

    Add sity

--- src/test/java/deliveryTest.java
+++ src/test/java/deliveryTest.java
@@ -1,15 +1,20 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class deliveryTest {
 
 
     @Test
     void test (){
+    Configuration.holdBrowserOpen=true;
+    //оставляет браузер открытым
     open ("http://localhost:9999/");
-
-
+//    $("[input__box]").setValue("Самара");
+//    $("[placeholder=\"Город\"]").setValue("Москва");
+//    $x(".//span[@data-test-id='city']//input").setValue("Самара");
+$("[data-test-id=city] input.input__control").setValue("Москва");
     }
 
 }
