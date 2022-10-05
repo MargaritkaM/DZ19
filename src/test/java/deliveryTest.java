d1d8de2fb7c267805e56c205bfc8b5fb2ab6805e
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-10-06 00:03:23 +0300

    Reformat code

--- src/test/java/deliveryTest.java
+++ src/test/java/deliveryTest.java
@@ -1,24 +1,26 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class deliveryTest {
 
 
     @Test
-    void test (){
-    Configuration.holdBrowserOpen=true;
-    //оставляет браузер открытым
-    open ("http://localhost:9999/");
-//    $("[input__box]").setValue("Самара");
-//    $("[placeholder=\"Город\"]").setValue("Москва");
-//    $x(".//span[@data-test-id='city']//input").setValue("Самара");
-    $("[data-test-id=city] input").setValue("Москва");
-//$("[data-test-id=city] input.input__control").setValue("Москва");
-    $("[data-test-id=date] input.input__control").setValue("02.10.2022");
-//    $x(".//span[@data-test-id='date']//input").setValue("03.10.2022");
+    void PositiveTestWithCssSelectors() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input.input__control").setValue("05.10.2022");
         $("[data-test-id=name] input").setValue("Мусатова Маргарита");
+        $("[data-test-id=phone] input").setValue("+79169044591");
+        $("[data-test-id='agreement']").click();
+        $("div>button").click();
+        $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
     }
-
 }
