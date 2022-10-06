30137d7aafa680392e31677ef2a04ea258b678f5
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-10-06 14:06:29 +0300

    Add test Xpass

--- src/test/java/deliveryTest.java
+++ src/test/java/deliveryTest.java
@@ -23,4 +23,17 @@ public class deliveryTest {
         $("div>button").click();
         $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
     }
+    @Test
+    void PositiveTestWithXpass (){
+        Configuration.holdBrowserOpen=true;
+        //оставляет браузер открытым
+        open ("http://localhost:9999/");
+        $x(".//span[@data-test-id='city']//input").setValue("Самара");
+        $x(".//span[@data-test-id='date']//input[@placeholder]").setValue("08.10.2022");
+        $x(".//span[@data-test-id='name']//input").setValue("Мусатова Маргарита");
+        $x(".//span[@data-test-id='phone']//input").setValue("+79169044591");
+        $x(".//label[@data-test-id='agreement']").click();
+        $x(".//div/button").click();
+        $x(".//div[@data-test-id='notification']").should(visible, Duration.ofSeconds(15));
+    }
 }
