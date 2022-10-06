67730553d617298b023bd4e4e27b6250c4f11b6b
Маргарита Мусатова <musatovamargarita@gmail.com>
2022-10-06 15:16:38 +0300

    Add Test Three City letters and choice calender data

--- src/test/java/deliveryTest.java
+++ src/test/java/deliveryTest.java
@@ -1,21 +1,25 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class deliveryTest {
 
-
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+    }
     @Test
     void PositiveTestWithCssSelectors() {
-        Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
         $("[data-test-id=date] input.input__control").setValue("05.10.2022");
         $("[data-test-id=name] input").setValue("Мусатова Маргарита");
@@ -26,9 +30,6 @@ public class deliveryTest {
     }
     @Test
     void PositiveTestWithXpass (){
-        Configuration.holdBrowserOpen=true;
-        //оставляет браузер открытым
-        open ("http://localhost:9999/");
         $x(".//span[@data-test-id='city']//input").setValue("Самара");
         $x(".//input[@placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
         $x(".//input[@placeholder='Дата встречи']").setValue("10.10.2022");
@@ -38,5 +39,27 @@ public class deliveryTest {
         $x(".//label[@data-test-id='agreement']").click();
         $x(".//div/button").click();
         $x(".//div[@data-test-id='notification']").should(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void PositiveTestWithCssSelectorThreeCityLettrs() {
+        $("[data-test-id=city] input").sendKeys("Мос");
+        $(byText("Москва")).click();
+        $("[data-test-id=date] input.input__control").setValue("05.10.2022");
+        $("[data-test-id=name] input").setValue("Мусатова Маргарита");
+        $("[data-test-id=phone] input").setValue("+79169044591");
+        $("[data-test-id='agreement']").click();
+        $("div>button").click();
+        $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void PositiveTestWithCssSelectorChoiceData() {
+        $("[data-test-id=city] input").setValue("Москва");
+        $("span>button").click();
+        $(byCssSelector("td[data-day='1665522000000']")).click();
+        $("[data-test-id=name] input").setValue("Мусатова Маргарита");
+        $("[data-test-id=phone] input").setValue("+79169044591");
+        $("[data-test-id='agreement']").click();
+        $("div>button").click();
+        $("[data-test-id='notification']").should(visible, Duration.ofSeconds(15));
     }
 }
